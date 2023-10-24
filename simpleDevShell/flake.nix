0349fbf9e76431fd2daba89c29d1858b650d617d
Gerg-L <GregLeyda@proton.me>
2023-10-24 11:52:18 -0400

    devShell: move pkgs

--- simpleDevShell/flake.nix
+++ simpleDevShell/flake.nix
@@ -1,13 +1,14 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-  outputs = {nixpkgs, ...}: {
-    devShells.x86_64-linux.default = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-      pkgs.mkShell {
-        packages = [
-          pkgs.hello
-        ];
-      };
-  };
+  outputs =
+    { nixpkgs, ... }:
+    {
+      devShells.x86_64-linux =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        {
+          default = pkgs.mkShell { packages = [ pkgs.hello ]; };
+        };
+    };
 }
